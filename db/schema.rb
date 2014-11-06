2e48a8925ea2254b8fe18251bebd63714d83fccc
Harrison Tsai <harrisontsai0123@gmail.com>
2014-11-05 20:35:03 -0800

    fix form and add time completed to donations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141106022944) do
+ActiveRecord::Schema.define(version: 20141106043330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20141106022944) do
     t.float    "latitude"
     t.float    "longitude"
     t.text     "additional_info"
+    t.datetime "completed"
   end
 
   add_index "donations", ["donor_id"], name: "index_donations_on_donor_id", using: :btree
