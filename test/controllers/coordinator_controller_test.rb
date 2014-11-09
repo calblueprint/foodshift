93dfd5798e37cbee3f8fb9b2182d8c485b1cfbde
akakitani <atsunori.kakitani@gmail.com>
2014-11-09 15:00:25 -0800

    Reorganize code for static pages

--- test/controllers/coordinator_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CoordinatorControllerTest < ActionController::TestCase
-  test "should get dashboard" do
-    get :dashboard
-    assert_response :success
-  end
-
-end
