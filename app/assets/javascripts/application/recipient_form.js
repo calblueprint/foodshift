6f945e2e9f18d698a45df18cadf227827191c4de
akakitani <atsunori.kakitani@gmail.com>
2014-11-23 14:56:26 -0800

    Add backend logic for recipient registration

--- app/assets/javascripts/application/recipient_form.js
+++ app/assets/javascripts/application/recipient_form.js
@@ -1,8 +1,17 @@
 $(function() {
-    new google.maps.places.Autocomplete(
+    var autocomplete = new google.maps.places.Autocomplete(
         (document.getElementById('recipient-address')), {
             types: ['geocode']
         });
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        getAddressCoordinates();
+    });
+
+    function getAddressCoordinates() {
+        var place = autocomplete.getPlace();
+        $('#recipient-address-lat').val(place.geometry.location.lat());
+        $('#recipient-address-lng').val(place.geometry.location.lng());
+    }
 
     $("#scroll-arrow").click(function() {
         $('html, body').animate({
@@ -17,4 +26,8 @@ $(function() {
                 }
             }
       });
+
+    $('#recipient-form-fields').on('invalid.fndtn.abide', function () {
+        toastr.error('There was an error with your submission');
+    });
 });
--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -5,5 +5,32 @@ class RecipientsController < ApplicationController
 
   # POST /receive
   def create
+    recipient_form = RecipientForm.new(recipient_params)
+    recipient_form.donor_id = current_user if user_signed_in?
+    respond_to do |format|
+      if recipient_form.create_objects
+        format.html { redirect_to root_path, notice: "You have been successfully registered." }
+      else
+        format.html { render recipients_new_path }
+      end
+    end
+  end
+
+  def recipient_params
+    params.require(:recipient).permit(
+      :email,
+      :password,
+      :password_confirm,
+      :first_name,
+      :last_name,
+      :phone,
+      :organization_name,
+      :address,
+      :latitude,
+      :longitude,
+      :organization_number,
+      :kitchen,
+      :refrigeration
+    )
   end
 end
