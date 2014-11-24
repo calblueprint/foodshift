480ef6f4f9ed53aec9055f36ec47f566caf041cf
Erica <eriicaericaerica@gmail.com>
2014-11-24 00:49:31 -0800

    more hound

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,54 +13,61 @@
 
 def create_donations
   1.upto(5) do |n|
-    Donation.create! organization: "Blueprint",
-    address: "Sutardja Dai Hall, Berkeley, CA 94709",
-    person: ["Alli", "Atsu", "Harrison", "Erica", "Quinton"].sample,
-    phone: "408-283-6923",
-    email: "foodshift@shiftfood.org",
-    refrigeration: [true, false].sample,
-    quantity: "10",
-    window_start: DateTime.new(2014, 8, 21, 8, 0),
-    window_end: DateTime.new(2014, 8, 21, 11, 0),
-    food_type: ["Bread", "Bulk", "Dairy", "Juice", "Mixed", "Meat", "Prepared", "Produce"].sample,
-    longitude: "37.8747924",
-    latitude: "-122.2583104"
+    Donation.create! 
+      organization: "Blueprint",
+      address: "Sutardja Dai Hall, Berkeley, CA 94709",
+      person: ["Alli", "Atsu", "Harrison", "Erica", "Quinton"].sample,
+      phone: "408-283-6923",
+      email: "foodshift@shiftfood.org",
+      refrigeration: [true, false].sample,
+      quantity: "10",
+      window_start: DateTime.new(2014, 8, 21, 8, 0),
+      window_end: DateTime.new(2014, 8, 21, 11, 0),
+      food_type: ["Bread", "Bulk", "Dairy", "Juice", "Mixed", "Meat", "Prepared", "Produce"].sample,
+      longitude: "37.8747924",
+      latitude: "-122.2583104"
   end
 end
 
 def create_users
   1.upto(5) do |n|
-    Donor.create! email: "donor#{n}@foodshift.net",
-    password: "password",
-    subscribed: "true",
-    type: "Donor"
+    Donor.create! 
+      email: "donor#{n}@foodshift.net",
+      password: "password",
+      subscribed: "true",
+      type: "Donor"
   end
 
   1.upto(5) do |n|
-    Recipient.create! email: "recipient#{n}@foodshift.net",
-    password: "password",
-    subscribed: "true",
-    type: "Recipient"
+    Recipient.create! 
+      email: "recipient#{n}@foodshift.net",
+      password: "password",
+      subscribed: "true",
+      type: "Recipient"
   end
 
   1.upto(5) do |n|
-    Coordinator.create! email: "coordinator#{n}@foodshift.net",
-    password: "password",
-    subscribed: "true",
-    type: "Coordinator"
+    Coordinator.create!
+      email: "coordinator#{n}@foodshift.net",
+      password: "password",
+      subscribed: "true",
+      type: "Coordinator"
   end
 
-  Donor.create! email: "ericayin@berkeley.edu",
+  Donor.create! 
+    email: "ericayin@berkeley.edu",
     password: "password",
     subscribed: "true",
     type: "Donor"
 
-  Recipient.create! email: "ericayin831@gmail.com",
+  Recipient.create! 
+    email: "ericayin831@gmail.com",
     password: "password",
     subscribed: "true",
     type: "Recipient"
 
-  Coordinator.create! email: "eriicaericaerica@gmail.com",
+  Coordinator.create! 
+    email: "eriicaericaerica@gmail.com",
     password: "password",
     subscribed: "true",
     type: "Coordinator"
