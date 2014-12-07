fc174aa774c89ecc2a9cd4f045bf49375e1b8dad
Erica <eriicaericaerica@gmail.com>
2014-12-06 20:36:06 -0800

    styling seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: "Chicago" }, { name: "Copenhagen" }])
-#   Mayor.create(name: "Emanuel", city: cities.first)
 
 def create_donations
   1.upto(5) do |n|
-    Donation.create!(organization: "Blueprint",
+    Donation.create!(
+    organization: "Blueprint",
     address: "Sutardja Dai Hall, Berkeley, CA 94709",
     person: ["Alli", "Atsu", "Harrison", "Erica", "Quinton"].sample,
     phone: "408-283-6923",
@@ -25,14 +21,16 @@ end
 
 def create_users
   1.upto(5) do |n|
-    Donor.create!(email: "donor#{n}@foodshift.net",
+    Donor.create!(
+    email: "donor#{n}@foodshift.net",
     password: "password",
     subscribed: "true",
     type: "Donor")
   end
 
   1.upto(5) do |n|
-    Recipient.create!(email: "recipient#{n}@foodshift.net",
+    Recipient.create!(
+    email: "recipient#{n}@foodshift.net",
     password: "password",
     subscribed: "true",
     type: "Recipient")
@@ -52,7 +50,8 @@ def create_users
     subscribed: "true",
     type: "Donor")
 
-  Recipient.create!(email: "ericayin831@gmail.com",
+  Recipient.create!(
+    email: "ericayin831@gmail.com",
     password: "password",
     subscribed: "true",
     type: "Recipient")
