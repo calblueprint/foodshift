689ba30863a5306c5551803768fd5c0082af1483
Allison Leong <allisonhleong@gmail.com>
2015-02-07 16:04:21 -0800

    Using annotate gem to annotate models

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: interests
+#
+#  id           :integer          not null, primary key
+#  created_at   :datetime
+#  updated_at   :datetime
+#  donation_id  :integer
+#  recipient_id :integer
+#
+
 class Interest < ActiveRecord::Base
   belongs_to :donation
   belongs_to :recipient
