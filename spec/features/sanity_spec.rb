da0d549b05b3f0a91005fe3401e5901f65e85fa9
Jason Won <jason.j.won@gmail.com>
2015-02-08 15:55:01 -0800

    oops forgot to track rspec files

---
+++ spec/features/sanity_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe "The app works" do
+  describe "if the home page" do
+    before { visit root_path }
+
+    it "works" do
+      expect(page).to have_content "Food Shift"
+    end
+  end
+end
