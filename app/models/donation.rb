2194a8d42e4344de5f583a65d60a0bcd467cde96
Allison Leong <allisonhleong@gmail.com>
2015-02-07 16:04:21 -0800

    Using annotate gem to annotate models

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: donations
+#
+#  id              :integer          not null, primary key
+#  created_at      :datetime
+#  updated_at      :datetime
+#  donor_id        :integer
+#  organization    :string(255)      not null
+#  address         :string(255)      not null
+#  person          :string(255)      not null
+#  phone           :string(255)      not null
+#  email           :string(255)      not null
+#  refrigeration   :boolean          default(FALSE)
+#  quantity        :string(255)      not null
+#  window_start    :datetime         not null
+#  window_end      :datetime         not null
+#  additional_info :text
+#  food_type       :string(255)      default([]), not null, is an Array
+#  latitude        :decimal(, )
+#  longitude       :decimal(, )
+#
+
 class Donation < ActiveRecord::Base
   belongs_to :donor
   nilify_blanks
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: transactions
+#
+#  id             :integer          not null, primary key
+#  created_at     :datetime
+#  updated_at     :datetime
+#  donation_id    :integer
+#  recipient_id   :integer
+#  coordinator_id :integer
+#  delivered_at   :datetime
+#  picked_up_at   :datetime
+#
+
 class Transaction < ActiveRecord::Base
   belongs_to :donation
   belongs_to :recipient
