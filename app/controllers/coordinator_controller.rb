7018254d08be96af0e40e8948fefa29348d58d92
akakitani <atsunori.kakitani@gmail.com>
2015-02-10 10:59:43 -0800

    Write queries for coordinator dashboard

--- app/controllers/coordinator_controller.rb
+++ app/controllers/coordinator_controller.rb
@@ -2,9 +2,16 @@ class CoordinatorController < ApplicationController
   before_action :auth
 
   def deliver
+    current_deliveries = Transaction.where(delivered_at: nil).joins(:donation, :recipient )
+    gon.deliveries = current_deliveries
   end
 
   def schedule
+    unique_interests = Interest.select(:donation_id).distinct
+    donations = Donation.where(id: unique_interests.pluck(:donation_id))
+    recipients = Recipient.where(id: unique_interests.pluck(:recipient_id))
+    gon.donations = donations
+    gon.recipients = recipients
   end
 
   def data
