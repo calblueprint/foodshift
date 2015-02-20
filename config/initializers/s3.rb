df09227fa6b5eecc4c1a44a5ddb9783c23c16875
Jason Won <jason.j.won@gmail.com>
2015-02-20 02:26:10 -0800

    Doge

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,12 +1,13 @@
 # Make sure these variables are set in .bash_profile or something.
-# Can later use Heroku to set ENV vars.  See https://devcenter.heroku.com/articles/config-vars
+# Later, for Heroku environment variables, check out:
+# https://devcenter.heroku.com/articles/config-vars
 
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => ENV['S3_KEY'],
-    :aws_secret_access_key  => ENV['S3_SECRET'],
-    :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
+    provider               : "AWS",
+    aws_access_key_id      : ENV["S3_KEY"],
+    aws_secret_access_key  : ENV["S3_SECRET"],
+    region                 : ENV["S3_REGION"]
   }
-  config.fog_directory  = ENV['S3_BUCKET']
+  config.fog_directory  = ENV["S3_BUCKET"]
 end
