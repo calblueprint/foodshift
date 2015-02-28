6c3aab5979dc3f437b698699c7c6ea2f426b8aee
Jason Won <jason.j.won@gmail.com>
2015-02-28 15:40:35 -0800

    updated figaro required vars

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,3 +1,3 @@
 # config/initializers/figaro.rb
 
-Figaro.require_keys("S3_KEY", "S3_SECRET", "S3_BUCKET", "S3_REGION")
+Figaro.require_keys("S3_KEY", "S3_SECRET", "S3_BUCKET", "S3_REGION", "EMAIL_USERNAME", "EMAIL_PASSWORD")
