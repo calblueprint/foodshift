1322e91f98cc240944c62b10f3cf298b3461e6c6
Jason Won <jason.j.won@gmail.com>
2015-03-01 14:40:25 -0800

    removed landing images, modified unsubscribe so it performs error checking

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,10 @@
 class ApiController < ApplicationController
   # GET /unsubscribe
   def unsubscribe
-    @user = User.where(:secret_token => params[:token]).first
-    @user.update_attributes(:subscribed => false)
-    redirect_to root_url, alert: "Successfully unsubscribed"
+    if User.exists?(:secret_token => params[:token])
+      @user = User.where(:secret_token => params[:token]).first
+      @user.update_attributes(:subscribed => false)
+      redirect_to root_url, alert: "Successfully unsubscribed"
+    end
   end
 end
