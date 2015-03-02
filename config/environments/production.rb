48968c7f7054121191befabb534e7990b059be50
Jason Won <jason.j.won@gmail.com>
2015-03-01 17:13:50 -0800

    modified Procfile, reverted changes to production.rb (since we shouldn't be using those test emails for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,17 +62,8 @@ Rails.application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              "smtp.gmail.com",
-    port:                 587,
-    domain:               "gmail.com",
-    user_name:            ENV["EMAIL_USERNAME"],
-    password:             ENV["EMAIL_PASSWORD"],
-    authentication:       "plain",
-    enable_starttls_auto: true  }
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
