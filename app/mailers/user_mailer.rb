3c25622899069c756282b591d2a111ce21079ff1
Jason Won <jason.j.won@gmail.com>
2015-03-01 17:57:38 -0800

    error checking in user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,11 +11,13 @@ class UserMailer < ActionMailer::Base
   end
 
   def recipient_match(recipients)
-    mail(to: recipients, subject: "Your donation has found a recipient!")
+    if recipients.length > 0
+      mail(to: recipients, subject: "Your donation has found a recipient!")
   end
 
   def coordinator_email(recipients, donation)
-    @donation = donation
-    mail(to: recipients, subject: "(Coordinator) Donation Posted!")
+    if recipient.length > 0
+      @donation = donation
+      mail(to: recipients, subject: "(Coordinator) Donation Posted!")
   end
 end
