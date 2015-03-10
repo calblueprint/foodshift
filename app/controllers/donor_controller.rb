51c45bace20bddc44e981b90e3b12c6409e987bf
Erica <eriicaericaerica@gmail.com>
2015-03-10 03:35:59 -0700

    add status to donations

--- app/controllers/donor_controller.rb
+++ app/controllers/donor_controller.rb
@@ -2,5 +2,8 @@ class DonorController < ApplicationController
 	def profile
 		@user = current_user
 		@donations = Donation.where("donor_id = ?", current_user.id)
+		@pending_donations = @donations.where("status = ?", 'Pending')
+		@inprogress_donations = @donations.where("status = ?", 'In Progress')
+		@completed_donations = @donations.where("status = ?", 'Complete')
 	end
 end
