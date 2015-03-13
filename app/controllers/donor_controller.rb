8fa6adce9dd33c91338558a4d028c37b86a1149c
Jason Won <jason.j.won@gmail.com>
2015-03-12 23:25:58 -0700

    fixed errors in donor_controller

--- app/controllers/donor_controller.rb
+++ app/controllers/donor_controller.rb
@@ -4,23 +4,23 @@ class DonorController < ApplicationController
   def profile
     @user = current_user
     @donations = Donation.where("donor_id = ?", current_user.id)
-    @pending_donations = @donations.where("status = ?", 'Pending')
-    @inprogress_donations = @donations.where("status = ?", 'In Progress')
-    @completed_donations = @donations.where("status = ?", 'Completed')
+    @pending_donations = @donations.where(:status => 'Pending')
+    @inprogress_donations = @donations.where(:status => 'In Progress')
+    @completed_donations = @donations.where(:status => 'Completed')
   end
 
   def find_transaction(donation_id)
-    return Transaction.where("donation_id = ?", donation_id).first
+    return Transaction.where(:donation_id => donation_id).first
   end
 
   def find_transaction_coordinator(donation_id)
-    @transaction = Transaction.where("donation_id = ?", donation_id).first
+    @transaction = Transaction.where(:donation_id => donation_id).first
     @coord_id = @transaction.coordinator_id
     return Coordinator.find(@coord_id)
   end
 
   def find_transaction_recipient(donation_id)
-    @transaction = Transaction.where("donation_id = ?", donation_id).first
+    @transaction = Transaction.where(:donation_id => donation_id).first
     @recipient_id = @transaction.recipient_id
     return Recipient.find(@recipient_id)
   end
