8150ee5eafc0681e5d7510a39231e13b50a0f670
Erica <eriicaericaerica@gmail.com>
2015-04-07 22:04:37 -0700

    recipient profile shows up yay

--- app/controllers/recipient_profiles_controller.rb
+++ app/controllers/recipient_profiles_controller.rb
@@ -1,4 +1,8 @@
 class RecipientProfilesController < ApplicationController
   def show
+  	@user = current_user
+  	@profile = RecipientProfile.find_by(recipient_id: current_user.id)  
+  	@received_donations = Transaction.where(recipient_id: current_user.id)
+  	@requested_donations = Interest.where(recipient_id: current_user.id)
   end
 end
