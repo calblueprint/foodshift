d8c950a6f6d75c1b055eec893da3e96a6c61fada
Jason Won <jason.j.won@gmail.com>
2015-04-21 21:11:24 -0700

    Creates a square thumbnail for logos

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -20,4 +20,24 @@ class LogoUploader < CarrierWave::Uploader::Base
   def extension_white_list
     %w(jpg jpeg gif png)
   end
+
+  version :thumb do
+    process resize_and_crop: 200
+  end
+
+  private
+  # Resize and crop square from Center
+  def resize_and_crop(size)
+    manipulate! do |image|
+      if image[:width] < image[:height]
+        remove = ((image[:height] - image[:width])/2).round
+        image.shave("0x#{remove}")
+      elsif image[:width] > image[:height]
+        remove = ((image[:width] - image[:height])/2).round
+        image.shave("#{remove}x0")
+      end
+      image.resize("#{size}x#{size}")
+      image
+    end
+  end
 end
