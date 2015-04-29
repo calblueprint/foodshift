20b51cabb750f92f1af185289976aa2f3826fe74
Erica <eriicaericaerica@gmail.com>
2015-04-28 21:15:55 -0700

    delete unnecessary comments from code

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -13,14 +13,11 @@ class DonationsController < ApplicationController
   # POST /donate
   def create
     donation_form = DonationForm.new(donation_params)
-    donation_form.donor = current_user.id # this should never be nil
+    donation_form.donor = current_user.id
     respond_to do |format|
       if donation_form.create_objects
         @donation = donation_form.donation
         @recipient_ids = Recipient.where(subscribed: true).pluck(:id)
-        # puts("\n \n ///////////////// RECIPIENT EMAILS ///////////// \n")
-        # puts(@recipient_ids) 
-        # puts("\n \n \n \n \n \n \n \n")
         UserMailer.donation_available(@recipient_ids, @donation).deliver
         @coordinators = Coordinator.pluck(:email)
         UserMailer.coordinator_email(@coordinators, @donation).deliver
